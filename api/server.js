4063d8ee204968a2515c49098d6d6e4da0e4cb7f
timothy hoover <bnhilliard>
2022-10-30 20:32:00 -0700

    update

--- api/server.js
+++ api/server.js
@@ -1,3 +1,51 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const Users = require('./users/model')
+const server = express()
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+
+server.get('/api/users', (req, res) => {
+    User.find()
+    .then(users => {
+        res.json(users)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "The user information could not be retrieved", 
+            err: err.message, 
+        })
+    })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    Users.findById(req.params.id)
+    .then(user => {
+        if (!user) {
+          res.status(404).json({
+        message: "The user with the specified ID does not exist" })
+        }
+        res.json(user)
+    })
+    .catch(err => {
+        res.status(500).json('Internal Server Error')
+    })
+})
+
+server.post('/api/users', (req, res) =>{
+    Users.insert(req.params)
+    .then(newUser => {
+        console.log(newUser)
+    })
+    .catch(err => {
+        if (!req.params.name || !req.params.bio)
+        res.status(500).json('Internal Server Error')
+    })
+})
+
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'not found'
+    })
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
